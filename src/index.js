a27d1e95bd6d13cc2d92227e15b82ffb5d177146
inaridarkfox4231 <ry-ota-10035-2m@ka.tnc.ne.jp>
2018-06-08 11:12:07 +0900

    計算機

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-
+function getValue(id){
+  return document.getElementById(id).value;
+}
+function setValue(id, n){
+  document.getElementById(id).value = n.toString();
+}
+function calc(kind){
+  var x = getValue("number1");
+  var y = getValue("number2");
+  x = Number(x);
+  y = Number(y);
+  if(isNaN(x) || isNaN(y)){ alert("inputError!"); return NaN; }
+  if(kind == "sum"){ return x + y; }
+  else if(kind == "subtract"){ return x - y; }
+  else if(kind == "product"){ return x * y; }
+}
+function result_show(kind){
+  var z = calc(kind);
+  if(!isNaN(z)){ setValue(kind, z); }
+}
+function calc_sum(){ result_show("sum"); }
+function calc_subtract(){ result_show("subtract"); }
+function calc_product(){ result_show("product"); }
