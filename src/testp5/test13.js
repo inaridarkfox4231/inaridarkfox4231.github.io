89e351ab40f923b2bb6a4b138a5f18bc1ab8859e
INARI_DARKFOX <39549290+inaridarkfox4231@users.noreply.github.com>
2023-01-18 09:32:19 +0900

    vertexColorやめる

---
+++ src/testp5/test13.js
@@ -0,0 +1,68 @@
+
+
+let gl;
+
+function setup() {
+  createCanvas(640, 640, WEBGL);
+	pixelDensity(1);
+
+  let geom = new p5.Geometry();
+
+  const v = createVector();
+  const setV = (x,y,z) => v.set(x,y,z).copy();
+
+  geom.vertices.push(
+		setV(-1,-1,1),setV(1,-1,1),setV(1,1,1),setV(-1,1,1),
+    setV(-1,-1,-1),setV(1,-1,-1),setV(1,1,-1),setV(-1,1,-1)
+  );
+  geom.vertexColors.push(
+    0,0,1,1, 0,1,0,1, 1,0,0,1, 1,1,1,1,
+    1,1,0,1, 1,0,1,1, 0,1,1,1, 1,1,1,1
+  );
+  geom.faces.push(
+		[0,1,2],[0,2,3],[4,7,6],[4,6,5],
+    [3,2,6],[3,6,7],[0,3,7],[0,7,4],
+    [2,1,5],[2,5,6],[1,0,4],[1,4,5]
+  );
+  geom.computeNormals();
+  this._renderer.createBuffers("myCube", geom);
+
+	gl = this._renderer.GL;
+}
+function draw(){
+  background(0);
+	resetMatrix();
+  rotateX(frameCount*TAU/240);
+  rotateY(frameCount*TAU/320);
+  directionalLight(255,255,255,0,0,-1);
+  ambientLight(64);
+  ambientMaterial(128);
+  noStroke();
+  fill(255);
+  this._renderer.drawBuffersScaled("myCube", 100, 100, 100);
+
+	resetMatrix();
+	translate(0, 100, 0);
+	fill(255);
+  box(90);
+	translate(0, -200, 0);
+	fill(255, 128, 0);
+	box(90);
+	translate(100, 100, 0);
+	fill(0, 128, 255);
+	sphere(100);
+	translate(-200, 0, 0);
+	fill(40, 222, 66);
+	const t = frameCount*TAU/300;
+	rotateX(t);
+	torus(90, 12);
+	rotateX(-t);
+}
+
+// こういうの用意するとインデックス考えるのが楽になります
+/*
+上面：0----1    側面：3---2   2---1
+      3----2          7---6   6---5
+下面：4----5    側面：0---3   1---0
+      7----6          4---7   5---4
+*/
