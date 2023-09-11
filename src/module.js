e6cf15aab1f77193a9392778cbbdfd463ef4bee4
INARI_DARKFOX <39549290+inaridarkfox4231@users.noreply.github.com>
2023-09-12 01:34:26 +0900

    export

--- src/module.js
+++ src/module.js
@@ -1,6 +1,6 @@
 // module.js
 import {testFunction} from "./testModule.js";
 
-const foxTestFunction = function(x, y, z){
+export const foxTestFunction = function(x, y, z){
   return testFunction(x, y, z);
 }
