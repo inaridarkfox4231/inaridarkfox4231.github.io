4a2fb387d7c1f123cbb0f6add795c405e401d723
INARI_DARKFOX <39549290+inaridarkfox4231@users.noreply.github.com>
2023-09-12 11:51:19 +0900

    p5の

--- src/module.js
+++ src/module.js
@@ -4,3 +4,9 @@ import {testFunction} from "./testModule.js";
 export const foxTestFunction = function(x, y, z){
   return testFunction(x, y, z);
 }
+
+// p5の関数使ってる場合とかそういう
+export const foxCircleDraw = function(){
+  fill("brown");
+  circle(200, 200, 400);
+}
